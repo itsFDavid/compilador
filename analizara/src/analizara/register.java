d1836e23689b8eed6497456dea34730acc729b15
DanielYeray5 <136346612+DanielYeray5@users.noreply.github.com>
2024-11-28 21:59:44 -0600

    correction about textfiel

--- analizara/src/analizara/register.java
+++ analizara/src/analizara/register.java
@@ -74,7 +74,7 @@ public class register extends javax.swing.JFrame {
 
         jLabel4.setText("Confirma la contraseña");
 
-        jTextField3.setText("jTextField3");
+        jTextField3.setText("");
         jTextField3.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jTextField3ActionPerformed(evt);
